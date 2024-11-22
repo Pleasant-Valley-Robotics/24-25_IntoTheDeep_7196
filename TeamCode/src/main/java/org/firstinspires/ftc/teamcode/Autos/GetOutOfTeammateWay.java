905722fb99b40cb950bb9bc33a06924e9a4fdbc8
BritneyBick <148918388+BritneyBick@users.noreply.github.com>
2024-11-22 14:57:40 -0600

    Made a auto to get out of our teammates way.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/GetOutOfTeammateWay.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/GetOutOfTeammateWay.java
@@ -3,12 +3,13 @@ package org.firstinspires.ftc.teamcode.Autos;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.robotcore.external.SignificantMotionDetection;
 import org.firstinspires.ftc.teamcode.Hardware.Drivebase;
 
-@Autonomous(name = "trainingAuto")
-public class trainingAuto extends LinearOpMode {
+@Autonomous(name = "GetOutOfWay")
+public class GetOutOfTeammateWay extends LinearOpMode {
     private double SIDE_SPEED = .2;
+    private double DRIVE_SPEED = .9;
+
 
     public void runOpMode() {
         Drivebase drivebase = new Drivebase(hardwareMap, this::opModeIsActive, telemetry);
@@ -16,6 +17,7 @@ public class trainingAuto extends LinearOpMode {
         waitForStart();
 
         //Put code here
-        drivebase.driveSideways(SIDE_SPEED, -8, 0);
+        drivebase.driveStraight(DRIVE_SPEED, 48, 0);
+        drivebase.sendTelemetry(true);
     }
 }
